bcb8a91b34e5030641c91fb99c968015f8ac4ea5
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-02-22 18:45:25 +0530

    jwt functionality implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
+import { jwtConstants } from './constants';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
