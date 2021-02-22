6575237995885958d7ca4fcdf7975766ecff0008
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-02-22 13:16:37 +0530

    tweet controller and service generated

--- src/tweet/tweet.module.ts
+++ src/tweet/tweet.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { TweetSchema } from './models/tweet.model';
+import { TweetController } from './tweet.controller';
+import { TweetService } from './tweet.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Tweet', schema: TweetSchema }]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [TweetController],
+  providers: [TweetService],
 })
 export class TweetModule {}
