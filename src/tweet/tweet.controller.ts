32fb692731c118902c21d4a3abfdd12eb4f3cd72
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-02-22 14:51:43 +0530

    create tweet api endpoint implemented

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
 import { TweetService } from './tweet.service';
 import { Tweet } from './interfaces/tweet.interface';
+import { CreateTweetDto } from './dto/createTweet.dto';
 
 @Controller('api/v1/tweets')
 export class TweetController {
@@ -9,5 +11,10 @@ export class TweetController {
   @Get()
   async getAllTweets(): Promise<Tweet[]> {
     return this.tweetService.getAllTweets();
+  }
+
+  @Post()
+  async createTweet(@Body() tweet: CreateTweetDto) {
+    return this.tweetService.createTweet(tweet);
   }
 }
--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
 import { Tweet } from './interfaces/tweet.interface';
+import { CreateTweetDto } from './dto/createTweet.dto';
 
 @Injectable()
 export class TweetService {
@@ -11,5 +14,12 @@ export class TweetService {
 
   async getAllTweets(): Promise<Tweet[]> {
     return await this.tweetModel.find().exec();
+  }
+
+  async createTweet(tweet: CreateTweetDto) {
+    tweet['tweetId'] = uuidv4();
+    tweet['author'] = 'altaf';
+    const createdTweet = new this.tweetModel(tweet);
+    return await createdTweet.save();
   }
 }
